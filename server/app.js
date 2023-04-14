54cfd1de1a8e87826545205c75c8f2dcd4c5c6d3
Dosemanai <dsemnai@gmail.com>
2023-04-15 00:42:24 +0900

    Install all necessary packages, move app.js to server

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
+const dir = path.resolve(__dirname);
 const app = express();
 
-app.use('/dist', express.static('dist'));
-app.use('/public', express.static('public'));
+app.use('/dist', express.static(path.join(dir, '..', 'dist')));
+app.use('/public', express.static(path.join(dir, '..','public')));
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.sendFile( path.resolve('./public/main.html'));
+  res.sendFile( path.join(dir, '..','public', 'main.html'));
 }); app.use('/', router);
 
 const port = process.env.PORT || 3001;
