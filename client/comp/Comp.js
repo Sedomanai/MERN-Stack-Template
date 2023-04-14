a870033619984b4e3d66f5b216d7c37c3efb9604
Dosemanai <dsemnai@gmail.com>
2023-04-15 01:47:12 +0900

    Add timer (for bsync response)

--- client/comp/Comp.js
+++ client/comp/Comp.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
+import moment from 'moment';
 
 export default function Comp() {
+  const [currentTime, setCurrentTime] = useState(moment());
+  
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(moment());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <>
         <p className='text-green-700 text-xl'>What's up?</p>
         <br/>
         <p>This is a component.</p>
-        <p>Right now it's {new Date().toDateString()} </p>
+        <p>Right now it's {currentTime.format('MMMM Do YYYY, h:mm:ss a')} </p>
         <p>How's coding going on?</p>
     </>
   );
